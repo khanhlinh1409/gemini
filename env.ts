e200cf49e638acdaaad6398af8fc3a288cd4a106
khanhlinh1409 <khanhlinh1409@gmail.com>
2025-04-15 13:07:52 +0700

    delete files

--- env.ts
+++
@@ -1,8 +0,0 @@
-// src/config/env.ts
-import * as dotenv from 'dotenv';
-import * as path from 'path';
-console.log('ENV loaded from:', path.resolve(__dirname, './.env'));
-
-dotenv.config({ path: path.resolve(__dirname, './.env') });
-console.log(process.env.API_KEY);
-export const API_KEY = process.env.API_KEY;
--- gemini1.ts
+++
@@ -1,3 +0,0 @@
-import { API_KEY } from './env';
-
-console.log('Your API_KEY:', API_KEY);
