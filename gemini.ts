96525c4cff9ffa99df18e65a9cb2bcc6c45c4535
khanhlinh1409 <khanhlinh1409@gmail.com>
2025-04-18 14:10:00 +0700

    updated

--- gemini.ts
+++ gemini.ts
@@ -1,18 +1,11 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import * as dotenv from 'dotenv';
 dotenv.config();
-// Replace with your actual API key
 require('dotenv').config();
 const API_KEY = process.env.API_KEY;
 
 
-// string getKey (){
-//   string localkeyvalue = keyfromlocaljsonfile();//o local thi key nay =kjdfu954oijkjkfdfj, tren server thi key = empty, file chua key nay nam trong lít gitignore
-//   if (localkeyvalue == empty)
-//     return process.env.API_KEY;
-//   else
-//     return localkeyvalue;
-// }
+
 async function generateText(prompt: string): Promise<string | null> {
   if (!API_KEY) {
     console.error("API key not found. Please set the GOOGLE_API_KEY environment variable.");
